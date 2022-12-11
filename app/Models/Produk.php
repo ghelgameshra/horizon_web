d22da2a5be658b98a7dbaa110f731a40493d5a83
ghelgameshra <ghelgameshra3347@gmail.com>
2022-12-11 14:25:45 +0700

    membenahi tampilan detail

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -10,7 +10,7 @@ class Produk extends Model
 {
     use HasFactory;
     use Sluggable;
-    protected $guarded = ['id', 'slug'];
+    protected $guarded = ['id'];
     protected $with = ['category', 'kategori'];
 
     public function scopeFilter($query, array $filters){
--- resources/views/dashboard/products/show.blade.php
+++ resources/views/dashboard/products/show.blade.php
@@ -4,39 +4,47 @@
     <h1 class="h2 fs-3">Add Product</h1>
 </div>
 
-<div class="col-lg-3">
-    <form method="post" action="/dashboard/products">
+<div class="fs-4 mb-4">
+    <a href="/dashboard/products/" class="badge bg-success"><i class="bi bi-arrow-left px-2 py-2"></i></a>
+    <a href="" class="badge bg-warning"><i class="bi bi-pencil-square px-2 py-2"></i></a>
+
+    <form action="/dashboard/products" method="post" class="d-inline">
+        @method('delete')
         @csrf
+        <button class="badge bg-danger border-0"><i class="bi bi-x-circle px-2 py-2"></i></button>
+    </form>
+</div>
+<div class="row">
+    <div class="col-lg-4">
         <div class="mb-2">
             <label for="name" class="form-label">Product Name</label>
-            <input type="text" class="form-control" id="name" name="name" autofocus value="{{ $product->name }}">
+            <input type="text" class="form-control" id="name" name="name" readonly value="{{ $product->name }}" disabled>
         </div>
-
+    
         <div class="mb-2">
             <label for="slug" class="form-label">Product Code</label>
-            <input type="text" class="form-control" id="slug" name="slug" readonly value="{{ $product->slug }}">
+            <input type="text" class="form-control" id="slug" name="slug" readonly value="{{ $product->slug }}" disabled>
         </div>
-
+    
         <div class="mb-2">
             <label for="price" class="form-label">Product Code</label>
-            <input type="text" class="form-control" id="price" name="price" value="@currency($product['price'])">
-
+            <input type="text" class="form-control disable" id="price" name="price" readonly value="@currency($product['price'])" disabled>
         </div>
+    </div>
+    <div class="col-md-4">
         <div class="mb-2">
             <label for="created_at" class="form-label">Created At</label>
-            <input type="text" class="form-control" id="created_at" name="created_at" value="{{ $product->created_at }}">
+            <input type="text" class="form-control" id="created_at" name="created_at" readonly value="{{ $product->created_at }}" disabled>
         </div>
         <div class="mb-2">
             <label for="updated_at" class="form-label">Updated At</label>
-            <input type="text" class="form-control" id="updated_at" name="updated_at" value="{{ $product->updated_at }}">
+            <input type="text" class="form-control" id="updated_at" name="updated_at" readonly value="{{ $product->updated_at }}" disabled>
         </div>
         
         <div class="mb-2">
             <label for="category_id" class="form-label">Category</label>
-            <input type="text" class="form-control" id="category_id" name="category_id" value="{{ $product->category->name }}">
+            <input type="text" class="form-control" id="category_id" name="category_id" readonly value="{{ $product->category->name }}" disabled>
         </div>
-        <button type="submit" class="btn btn-primary">Add product</button>
-    </form>
+    </div>
 </div>
-
 @endsection
